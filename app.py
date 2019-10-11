1906088394575f2de6c2cfb1917e7b8e1d4597b4
hrafnara <hrafn02@gmail.com>
2019-10-11 09:42:10 +0000

    ok

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-f
+from flask import Flask, flash, redirect, render_template, request, json
+import os
+import urllib.request
+from jinja2 import ext
+from Datatime import datatime
+
+app = Flask(__name__)
+
+with urllib.request.urlopen("http://apis.is/petrol") as url:
+    gogn = json.loads(url.read().decode)

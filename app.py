59dc2546c36b9fcab7ac1cc7ecfeb9e94ae3dd3d
hrafnara <hrafn02@gmail.com>
2019-10-27 16:36:34 +0000

    ok fin

--- app.py
+++ app.py
@@ -8,29 +8,83 @@ app = Flask(__name__)
 
 with urllib.request.urlopen("http://apis.is/petrol") as url:
     gogn = json.loads(url.read().decode())
-#    print(gogn)
+    li = gogn['results']
 
 app.jinja_env.add_extension(ext.do)
 
 def minPetrol():
-    return 100
-#    minPetrol = 1000
-#    company = None
-#    adress = None
-#    lst = gogn['results']
-#    for i in lst:
-#        if i['bensin95'] is not None:
-#            pass
+    minverd = 10000000000
+    for x in li:
+        if x['bensin95'] < minverd:
+            nafn = x['company']
+            minverd = x['bensin95']
+    return [minverd ,nafn]
+
 def minDiesel():
-    return 1000
+    minverd = 10000000000
+    for x in li:
+        if x['diesel'] < minverd:
+            minverd = x['diesel']
+            nafn =x['company']
+    return [minverd, nafn]
+
+def stadir():
+    stodvar = {}
+    for x in li:
+        nafn = x["company"]
+        if nafn not in stodvar:
+            stodvar[nafn] = [{
+                "bensin": x["bensin95"],
+                "diesel": x["diesel"],
+                "geo": x["geo"],
+                "nafn": x["name"],
+                "company": x["company"]
+                }]
+        else:
+            stodvar[nafn].append({
+                "bensin":x["bensin95"],
+                "diesel":x["diesel"],
+                "geo":x["geo"],
+                "nafn":x["name"],
+                "company":x["company"]})
+    return stodvar
+
+
 
 #-----------------------routes--------------------------------------
 
 @app.route("/")
 def index():
-    return render_template("index.tpl",gogn =gogn, minP=minPetrol(), minD=minDiesel())
+    oneco = []
+    for x in li:
+        if x['company'] not in oneco:
+            oneco.append(x['company'])
 
+    return render_template("index.html",oneco = oneco, minP=minPetrol(), minD=minDiesel())
+
+def header():
+    pass
+
+
+@app.route("/company/<nafn>")
+def fyrirtaeki(nafn):
+    listi = stadir()
+    return render_template("stadir.html",nafn = nafn, stodvar = listi[nafn])
+
+@app.route("/<company>/<nafn>")
+def stod(nafn,company):
+    listi = stadir()[company]
+    for i in listi:
+        if i["nafn"] == nafn:
+            dic = i
+            break
+    return render_template("stod.html",nafn=dic["nafn"],lat=dic["geo"]["lat"],lon=dic["geo"]["lon"],company = company, bensin=dic["bensin"],diesel=dic["diesel"])
+
+@app.errorhandler(404)
+def pagenotfound(error):
+    return render_template("404.html"), 404
 
 #-------------------run---------------------------------------------------
 if __name__ == "__main__":
     app.run(debug=True)
+
--- templates/company.tpl
+++
@@ -1,4 +0,0 @@
-{% exstends "base.html"%}
-{% block title%}Nánari uplýsingar bensínstöðvar{% endblock %}
-{% block content %}
-<div class="wrapper"s
